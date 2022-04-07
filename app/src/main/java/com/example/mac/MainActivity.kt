f653d372bef10db20b818ae5a159bdaf22d025cb
Mrblaszka <mateosblaszka@outlook.com>
2022-04-07 13:18:24 +0200

    Finish project

--- app/src/main/java/com/example/mac/MainActivity.kt
+++ app/src/main/java/com/example/mac/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.mac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val img = findViewById<ImageView>(R.id.imageView)
+        img.setImageResource(R.drawable.staw)
+        var i = 1
+
+        val kolejny = findViewById<Button>(R.id.button)
+        val poprzedni = findViewById<Button>(R.id.button2)
+
+        kolejny.setOnClickListener {
+            i++
+            if(i == 2){
+                img.setImageResource(R.drawable.miegusz)
+            }
+            if(i == 3){
+                img.setImageResource(R.drawable.kazalnica)
+            }
+        }
+        poprzedni.setOnClickListener{
+            i--
+            if(i == 3){
+                img.setImageResource(R.drawable.miegusz)
+            }
+            if(i == 2){
+                img.setImageResource(R.drawable.staw)
+            }
+            if(i == 1){
+                img.setImageResource(R.drawable.kazalnica)
+            }
+        }
     }
 }
